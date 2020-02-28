6933a2335e9ef49397a947f9cb5090ba60a86312
Ali Ulgr <aliulger15@gmail.com>
2020-02-28 13:42:44 +0300

    node week2 exercises and project

---
+++ week2/nodejs-exercises/exercise1/app.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express()
+const fs = require("fs");
+app.use(express.json());
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(3000);
+
+app.post('/blogs', (req, res) => {
+  // How to get the tile and content from the request??
+  const title = req.body.title;
+  const content = req.body.content;
+  fs.writeFileSync(title, content);
+  res.end('ok')
+})
+
+app.put('/blogs', (req, res) => {
+  // How to get the tile and content from the request??
+  const title = req.body.title;
+  const content = req.body.content;
+  if (fs.existsSync(title)) {
+    fs.writeFileSync(title, content);
+    res.end('it is updated')
+  }
+  else {
+    res.end('post does not exist');
+  }
+})
+
+app.delete('/blogs/:title', (req, res) => {
+  const title = req.params.title;
+  // How to get the tilte from the url parameters?
+  fs.unlinkSync(title);
+  res.end('it is deleted');
+})
+
+app.get('/blogs/:title', (req, res) => {
+  const title = req.params.title;
+  // How to get the tilte from the url parameters?
+  res.sendfile(title);
+})
